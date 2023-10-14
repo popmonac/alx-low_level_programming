a5e38e40ac7b01c48291dd7dde709ed2789a2e55
popmonac <lawsonthompson57@gmail.com>
2023-10-14 16:37:57 +0100

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+/**
+ * main - A program thats prints on the terminal using printf
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
