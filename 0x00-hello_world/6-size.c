6b859577bdbe7ef62569d7b0ea45e56d20266739
popmonac <lawsonthompson57@gmail.com>
2023-10-14 20:56:57 +0100

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - A program that shows the sizes of various types
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a char: %lu\n", sizeof(char));
+printf("The size of int is: %lu\n", sizeof(int));
+printf("The size of long int is: %lu\n", sizeof(long int));
+printf("The size of float is: %lu\n", sizeof(float));
+return (0);
+}
