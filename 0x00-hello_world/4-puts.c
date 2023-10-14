392deb20403b26ccb1b9924a6a1773a241462414
popmonac <lawsonthompson57@gmail.com>
2023-10-14 15:47:06 +0100

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,11 @@
-#!/bin/bash
-#include <stdio>
-
-/*
-*This program uses puts to print output to the terminal
-* int main(void){
-* puts ("\"programming is like building a multilingual puzzle");
-* return(0);*
-*/
-
-int main(void){
-
-puts("\"Programming is like building a multilingual puzzle")	
-	
+#include <stdio.h>
+/**
+ * Main- This program uses puts to print output 
+ * return 0 (success);
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
 return(0);
 }
 
