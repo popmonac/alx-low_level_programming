e358d38e3091bbd04819c148e55ef89bc998d9e8
popmonac <lawsonthompson57@gmail.com>
2023-10-15 22:20:09 +0100

    Task 3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio>
+/**
+ * main - Entry point for the alphabet code
+ *
+ * Return: (0) (Success)
+ */
+int main(void)
+{
+	char a;
+
+	a = 'a';
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+	return (0);
+}
