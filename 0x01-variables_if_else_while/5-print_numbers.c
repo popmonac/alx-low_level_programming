c2d6b89294a9569c6b637dc6749a7ad1da9e79ed
popmonac <lawsonthompson57@gmail.com>
2023-10-16 07:06:50 +0100

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point for the code
+ *
+ * Return: (0) (Success)
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		printf("%d", n);
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
