0e7162c984a66f25f1f495fa78afe94740c95484
popmonac <lawsonthompson57@gmail.com>
2023-10-16 06:44:09 +0100

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point for the code
+ *
+ * Return: (0) (Success)
+ */
+int main(void)
+{
+	char l;
+
+	l = 'a';
+	while (l <= 'z')
+	{
+		if (l == 'e' || l == 'q')
+			l++;
+		putchar(l);
+		l++;
+	}
+	putchar(l);
+	return (0);
+}
