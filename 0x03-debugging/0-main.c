b03521104aa05e454e81010bb607080ff81cfaf6
popmonac <lawsonthompson57@gmail.com>
2023-10-18 15:07:07 +0100

    Task

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* main -This tests function prints if an integer is positive or negative
+* Return: 0
+*/
+
+int main(void)
+{
+        int i;
+
+        i = 98;
+        positive_or_negative(i);
+
+        return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+This is my submission on the debugging task
