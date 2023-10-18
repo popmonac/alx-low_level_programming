da0155afa49afcb9812f651b2e5471ff55ab0915
popmonac <lawsonthompson57@gmail.com>
2023-10-18 04:59:27 +0100

    Task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * 
+ * swap_int - This function swap the contents of two variables
  *
+ * @a: this gets the argument stated in main.c
+ * @b: gets the argument in main.c
  */
 void swap_int(int *a, int *b)
 {
