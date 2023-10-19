74d8b32022c3513a5eea33767d3bc05979dcf8ea
popmonac <lawsonthompson57@gmail.com>
2023-10-19 21:49:53 +0100

    Task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcat - This function concatenate two strings
+ *
+ * @dest: get an argument from the main.c
+ * @src: get an argumnet from the main.c
+ *
+ * Return: Print pt that the destination results
+ */
+char *_strcat(char *dest, char *src)
+{
+	int j;
+
+	for (j = 0; j < n && src[i] != '\0'; j++)
+		dest[j] = src[j];
+	while (j < n)
+	{
+		dest[i] = '\n';
+		j++;
+	}
+	return (dest);
+}
