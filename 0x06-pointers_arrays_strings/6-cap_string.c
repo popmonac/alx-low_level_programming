7efa90aa96f141ca683d063c17b208945a64a5dd
Ayodeji Taofeek Khalid <78390497+popmonac@users.noreply.github.com>
2023-10-20 17:43:35 +0100

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,40 +1,7 @@
 int string_spacing(char chr);
 #include <stdio.h>
 #include "main.h"
-/**
- * cap_string - This function which Changes the first character to a string
- *
- * @wrd: gets the argument from main.c
- *
- * Return: Returns the outcome
- */
-char *cap_string(char *wrd)
-{
-	char *word = wrd;
-	int i = 0;
 
-	while (wrd[i])
-	{
-		if (i == 0 && (wrd[i] >= 'a' && wrd[i] <= 'z'))
-		{
-			wrd[i] -= 32;
-		}
-		else if (string_spacing(wrd[i]) && wrd[i + 1] >= 'a' && wrd[i + 1] <= 'z')
-		{
-			wrd[i + 1] -= 32;
-		}
-		i++;
-	}
-	return (word);
-}
-
-/**
- * string_spacing - This funtion create spacing between the string
- *
- * @chr: gets the spacing for cap_string
- *
- * Return: (1) (Success)
- */
 int string_spacing(char chr)
 {
 	int j;
