4ca6028b01ae38c2b6c88f72b9f550c231414b77
popmonac <lawsonthompson57@gmail.com>
2023-10-20 06:41:40 +0100

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,10 +11,11 @@
 int _strcmp(char *s1, char *s2)
 {
 	int j;
+	
 	for (j = 0; s1[j] != '\0' || s2[j] != '\0'; j++)
 	{
 		if (s1[j] != s2[j])
-		{	
+		{
 			if (s1[j] < s2[j])
 				return (s1[j] - s2[j]);
 			else if (s2[j] < s1[j])
