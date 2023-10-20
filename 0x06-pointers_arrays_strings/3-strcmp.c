a8fdaf05d59995a177e5e01219863a658fc72749
popmonac <lawsonthompson57@gmail.com>
2023-10-20 06:36:30 +0100

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,35 +10,15 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, j, letter = 15, len_i, len_j;
-	char word_a, word_b;
-
-	while (*s1 != '\0' && *s2 != '\0')
+	int j;
+	for (j = 0; s1[j] != '\0' || s2[j] != '\0'; j++)
 	{
-		len_i++;
-		len_j++;
-		s1++;
-		s2++;
-	}
-	for (i = 0; i < len_i; i++)
-	{
-		for (j = 0; j < len_j; j++)
-		{
-			word_i = s1[i];
-			word_j = s2[i];
-			if (word_i < word_j)
-			{
-				ans *= -1;
-				return (ans);
-			}
-			else if (word_i > word_j)
-			{
-				return (letter);
-			}
-			else
-			{
-				return (0);
-			}
+		if (s1[j] != s2[j])
+		{	
+			if (s1[j] < s2[j])
+				return (s1[j] - s2[j]);
+			else if (s2[j] < s1[j])
+				return (s1[j] - s2[j]);
 		}
 	}
 	return (0);
