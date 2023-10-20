73618e91918544bfc86af5ef7876d7e9d9a8c2ad
Ayodeji Taofeek Khalid <78390497+popmonac@users.noreply.github.com>
2023-10-20 16:42:45 +0100

    Update 1-strncat.c
    "task 1"

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,13 +11,13 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int len, i;
+        int len, i;
 
-	for (len = 0; dest[len] != '\0'; len++)
+        for (len = 0; dest[len] != '\0'; len++)
         {
         }
 
-	for (i = 0; i < n && src[i] != 0; i++)
+        for (i = 0; i < n && src[i] != 0; i++)
         {
                 dest[len + i] = src[i];
         }
