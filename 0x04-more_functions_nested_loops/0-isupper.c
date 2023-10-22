7f276e6be55f3d4170f9f7a920ca694e057145f3
popmonac <lawsonthompson57@gmail.com>
2023-10-23 00:30:44 +0100

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * _isupper - This Function checks for Uppercase and Lowercase
+ *
+ * @i: gets the argument from main.c
+ * Return: (0)
+ */
+int _isupper(int i)
+{
+	if (isupper(i))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is the answer to More functions, more nested loops
