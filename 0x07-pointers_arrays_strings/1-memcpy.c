40f536e0ee4f92bcd176637fad6e3f8f10c6225f
popmonac <lawsonthompson57@gmail.com>
2023-10-23 22:07:43 +0100

    More more more pointers

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _memcpy - This funtion copies memory area
+ * @dest: This argument filled with copied memory
+ * @src: This argument fill the 'dest'
+ * @n: Number of bytes
+ *
+ * Return: The filled 'dest'.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	dest = *src;
+
+	while (n--)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	return (book);
+}
