998d012a8331652398982bdee7b50460e2089c52
popmonac <lawsonthompson57@gmail.com>
2023-10-23 21:49:22 +0100

    More more more pointers

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _memset - This function fills memory with a constant byte
+ *
+ * @s: gets an argument 
+ * @b: gets an argumenet that fills 's'
+ * @n: Number of byte
+ *
+ * Return: The byte stored in 'book'
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *book = s;
+
+	while (n--)
+	{
+		*s = b;
+		s++;
+	}
+	return (book);
+}
