0b47851e62f7f2756931eb94997c6d4e8f6a7b0d
popmonac <lawsonthompson57@gmail.com>
2023-10-24 23:42:33 +0100

    Task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,17 +1,40 @@
 #include "main.h"
 /**
  * _strcpy - This Function copies the string pointed to by src,
- *
- * @dest: A pointer to destination of string
- * @src: A pointer to source string to copy from
- *
- * Return: pointer to dest
- */
-char *_strcpy(char *dest, char *src)
-{
-	char *letter = dest;
+ *#include <stdio.h>
 
-	while (*src)
-		*dest++ = *src++;
-	return (letter);
+/**
+ * _atoi - This function converts string to integer
+ * @k: gets the argument
+ * Return: integer from conversion
+ */
+int _atoi(char *k)
+{
+	int sign = 1;	
+	unsigned int total = 0;
+	char break_signal = 0;
+
+	while (*k)
+	{
+		if (*k == '-')
+			sign *= -1;
+
+		if (*k >= '0' && *k <= '9')
+		{
+			break_signal = 1;
+			total = total * 10 + *k - '0';
+		}
+
+		else if (break_signal)
+		{
+			break;
+		}
+		k++;
+	}
+
+	if (sign < 0)
+		total = total * -1;
+
+	return (total);
 }
+
