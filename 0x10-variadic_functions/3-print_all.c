4353be7c97541fdbe03066de330080abc8a800af
popmonac <lawsonthompson57@gmail.com>
2023-11-10 05:26:47 +0100

    Task

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,49 +1,32 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
+#include <ctype.h>
 
 /**
- * print_all - This prints all
+ * print_all - prints all
  * @format: param
  */
+
 void print_all(const char * const format, ...)
 {
-	va_list valist;
-	unsigned int a = 0, j, c = 0;
-	char *str;
-	const char t_arg[] = "cifs";
+	int a, b;
+	char *word;
 
-	va_start(valist, format);
-	while (format && format[a])
+	va_list args;
+
+	va_start(args, format);
+
+	for (a = 0; format[a]; a++)
 	{
-		j = 0;
-		while (t_arg[j])
+		word = va_arg(args, char *);
+		if (isdigit(word))
 		{
-			if (format[a] == t_arg[j] && c)
-			{
-				printf(", ");
-				break;
-			} j++;
+			_putchar(*word);
+			continue;
 		}
-		switch (format[a])
-		{
-			case 'c':
-				printf("%c", va_arg(valist, int)), c = 1;
-				break;
-			case 'a':
-				printf("%d", va_arg(valist, int)), c = 1;
-				break;
-			case 'f':
-				printf("%f", va_arg(valist, double)), c = 1;
-				break;
-			case 's':
-				str = va_arg(valist, char *), c = 1;
-				if (!str)
-				{
-					printf("(nil)");
-					break;
-				}
-				printf("%s", str);
-				break;
-		} a++;
+
+		for (b = 0; word[a]; b++)
+			_putchar(word[a]);
 	}
-	printf("\n"), va_end(valist);
+	va_end(args);
 }
